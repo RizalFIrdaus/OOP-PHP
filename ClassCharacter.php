851c5712dd3f8b05b70c1b840f07db3595e8e5ed
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-14 12:01:30 +0700

    Function

--- ClassCharacter.php
+++ ClassCharacter.php
@@ -10,4 +10,36 @@ class Character
         $facc,
         $sacc,
         $talisman;
+
+    function HealthPoint(int $value): int
+    {
+        $hp = $value;
+        return $hp;
+    }
+    function Job(int $value): int
+    {
+        switch ($value) {
+            case $value >= 0 && $value < 10000:
+                $job_level = 1;
+                return $job_level;
+                break;
+            case $value < 50000:
+                $job_level = 2;
+                return $job_level;
+                break;
+        }
+    }
+    function Base(int $value): int
+    {
+        switch ($value) {
+            case $value >= 0 && $value < 10000:
+                $base_level = 1;
+                return $base_level;
+                break;
+            case $value < 50000:
+                $base_level = 2;
+                return $base_level;
+                break;
+        }
+    }
 }
--- Manipulation.php
+++ Manipulation.php
@@ -11,15 +11,7 @@ $mage->facc = "Acc";
 $mage->sacc = "Acc";
 $mage->talisman = "Talisman";
 
-$swordman = new Character();
-$swordman->weap = "Sword";
-$swordman->shield = "Sword";
-$swordman->armor = "Armor";
-$swordman->muffler = "Muffler";
-$swordman->shoes = "Shoes";
-$swordman->facc = "Acc";
-$swordman->sacc = "Acc";
-$swordman->talisman = "Talisman";
+
 
 
 echo "Character : Mage" . PHP_EOL;
@@ -31,13 +23,6 @@ echo "Shoes : $mage->weap" . PHP_EOL;
 echo "Accessories : $mage->facc" . PHP_EOL;
 echo "Accessories : $mage->sacc" . PHP_EOL;
 echo "Talisman : $mage->talisman" . PHP_EOL;
-echo PHP_EOL;
-echo "Character : Swordman" . PHP_EOL;
-echo "Weapon : $swordman->weap" . PHP_EOL;
-echo "Shield : $swordman->shield" . PHP_EOL;
-echo "Armor : $swordman->armor" . PHP_EOL;
-echo "Muffler : $swordman->weap" . PHP_EOL;
-echo "Shoes : $swordman->weap" . PHP_EOL;
-echo "Accessories : $swordman->facc" . PHP_EOL;
-echo "Accessories : $swordman->sacc" . PHP_EOL;
-echo "Talisman : $swordman->talisman" . PHP_EOL;
+echo "Health : " . $mage->HealthPoint(20000) . PHP_EOL;
+echo "Job Level : " . $mage->Job(200) . PHP_EOL;
+echo "Base Level : " . $mage->Base(200) . PHP_EOL;
