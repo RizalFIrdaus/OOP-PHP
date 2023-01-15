9a9e95cf8e1e323772019bb2e21367d8441c4f6a
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-15 17:38:24 +0700

    import namespace from keyword use

---
+++ FunctionNamespace.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace helper;
+
+function helper()
+{
+    echo "This is helper" . PHP_EOL;
+}
+
+const APPLICATION = "Belajar OOP PHP";
--- Namespace.php
+++ Namespace.php
@@ -1,12 +1,17 @@
 <?php
 
 require_once "Conflict.php";
+require_once "FunctionNamespace.php";
 
 use \Data\C1\Conflict as Sum;
 use \Data\C2\Conflict as Sub;
+use function helper\helper;
+use const helper\APPLICATION;
 
 $summary = new Sum(12, 12);
 $subtraction = new Sub(12, 12);
 
 echo "Summary : $summary->a + $summary->b = " . $summary->sum() . PHP_EOL;
 echo "Subtraction : $subtraction->a - $subtraction->b = " . $subtraction->substraction() . PHP_EOL;
+echo APPLICATION . PHP_EOL;
+helper();
