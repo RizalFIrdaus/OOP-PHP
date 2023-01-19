b9e1f406d38e9dd76893d5e5306282b8e43450fa
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-19 21:23:29 +0700

    Final Keyword

---
+++ Final.php
@@ -0,0 +1,69 @@
+<?php
+
+
+trait Data
+{
+    public string $name;
+    public string $url;
+}
+interface Auth
+{
+    public function login(): void;
+    public function register(): void;
+    public function logout(): void;
+}
+
+
+class SocialMedia implements Auth
+{
+    use Data;
+
+    public function login(): void
+    {
+        echo "$this->name Login Auth" . PHP_EOL;
+    }
+
+    public function register(): void
+    {
+        echo "$this->name Register Auth" . PHP_EOL;
+    }
+
+    public function logout(): void
+    {
+        echo "$this->name Logout Auth" . PHP_EOL;
+    }
+}
+
+final class Facebook extends SocialMedia
+{
+    public function getName()
+    {
+        echo $this->name . PHP_EOL;
+    }
+    public function getUrl()
+    {
+        echo $this->url . PHP_EOL;
+    }
+}
+final class Instagram extends SocialMedia
+{
+    public function getName()
+    {
+        echo $this->name . PHP_EOL;
+    }
+    public function getUrl()
+    {
+        echo $this->url . PHP_EOL;
+    }
+}
+
+$facebook = new Facebook();
+$facebook->name = "Facebook";
+$facebook->url = "http://facebook.com";
+$facebook->getName();
+$facebook->getUrl();
+$instagram = new Instagram();
+$instagram->name = "Instagram";
+$instagram->url = "http://instagram.com";
+$instagram->getName();
+$instagram->getUrl();
