54a135c8c094181ce35578654758ebb2f5ddbbc9
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-21 17:18:14 +0700

    Clone Magic Function to ignore some properties to clone

--- Generator.php
+++ Generator.php
@@ -14,6 +14,12 @@ $gen = new class implements getData
 {
     use data;
 
+    // Tidak boleh di clone
+    public function __clone()
+    {
+        unset(self::$array);
+    }
+
     // Without Generator
     // Menggunakan array untuk passing data ke array iterator
     public function getGanjil(int $max): Iterator
@@ -54,13 +60,14 @@ $gen = new class implements getData
 $ganjil = $gen->getGanjil(100);
 $genap = $gen->getGenap(100);
 $clone = clone $ganjil;
+var_dump($ganjil);
 // foreach ($ganjil as $data) {
 //     echo "Ganjil : $data" . PHP_EOL;
 // }
 // foreach ($genap as $data) {
 //     echo "Genap : $data" . PHP_EOL;
 // }
-$oddeven = $gen->oddEven(100);
-foreach ($oddeven as $property => $value) {
-    echo "$property -> $value" . PHP_EOL;
-}
+// $oddeven = $gen->oddEven(100);
+// foreach ($oddeven as $property => $value) {
+//     echo "$property -> $value" . PHP_EOL;
+// }
