f097a15056105e2ae48f1973831c6edbfd83e0ce
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-21 14:51:56 +0700

    Generator

---
+++ Generator.php
@@ -0,0 +1,50 @@
+<?php
+
+
+trait data
+{
+    protected $array = [];
+}
+interface getData
+{
+    public function getGanjil(int $max): Iterator;
+}
+
+$gen = new class implements getData
+{
+    use data;
+
+    // Without Generator
+    // Menggunakan array untuk passing data ke array iterator
+    public function getGanjil(int $max): Iterator
+    {
+        for ($i = 1; $i < $max; $i++) {
+            if ($i % 2 == 1) {
+                $array[] = $i;
+            }
+        }
+        return new ArrayIterator($array);
+    }
+
+
+    // With Generator
+    // Tidak perlu menggunakan data array cukup menggunakan keyword yield
+    public function getGenap(int $max): Iterator
+    {
+        for ($i = 1; $i < $max; $i++) {
+            if ($i % 2 == 0) {
+                yield $i;
+            }
+        }
+    }
+};
+
+// Instansiasi Object
+$ganjil = $gen->getGanjil(100);
+$genap = $gen->getGenap(100);
+foreach ($ganjil as $data) {
+    echo "Ganjil : $data" . PHP_EOL;
+}
+foreach ($genap as $data) {
+    echo "Genap : $data" . PHP_EOL;
+}
