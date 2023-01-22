a8f12eb547029a77251af215d433019c73300314
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-23 00:25:39 +0700

    Contravariance

--- Animals.php
+++ Animals.php
@@ -4,6 +4,7 @@ abstract class Animals
 {
     public string $name;
     abstract public function run(): void;
+    abstract public function eat(AnimalFood $food): void;
 }
 
 class Cat extends Animals
@@ -12,11 +13,19 @@ class Cat extends Animals
     {
         echo "$this->name is running!";
     }
+    public function eat(AnimalFood $food): void
+    {
+        echo "$food";
+    }
 }
 class Dog extends Animals
 {
     public function run(): void
     {
         echo "$this->name is running!";
+    }
+    public function eat(Food $food): void
+    {
+        echo "$food";
     }
 }
---
+++ Food.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Food
+{
+}
+
+class AnimalFood extends Food
+{
+}
