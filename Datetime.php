a2e34e210c3e6591f381d0980b8dcbf048691f0f
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-23 01:05:02 +0700

    Datetime

--- Datetime.php
+++ Datetime.php
@@ -5,4 +5,14 @@
 $data = new DateTime();
 $data->setDate(2000, 05, 30);
 $data->setTime(1, 10, 0);
+
+// Date Interval = Penambahan DateTime
+
+// Untuk menambahkan interval
+$data->add(new DateInterval("P1Y"));
+// Untuk mengurangi Interval
+$minus = new DateInterval("P1M");
+$minus->invert = true;
+$data->add($minus);
+
 var_dump($data);
