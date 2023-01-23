9d53a0e054c107979a358339672c5a6d0223834c
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-23 22:17:56 +0700

    Try Catch getmessage and tracing error

--- Exeption.php
+++ Exeption.php
@@ -5,8 +5,18 @@ require_once "Validation.php";
 require_once "ValidationExeption.php";
 
 
-$login = new LoginRequest();
-$login->username = "admin";
-$login->password = "secret";
 
-validateLoginRequest($login);
+// TRY CATCH = walaupun error tetap dijalankan
+try {
+    $login = new LoginRequest();
+    // $login->username = "admin";
+    // $login->password = "123";
+    validateLoginRequest($login);
+    echo "Valid" . PHP_EOL;
+} catch (ValidationExeption | Exception  $exception) {
+    echo "Error : {$exception->getMessage()}" . PHP_EOL;
+    // get trace untuk melakukan tracing pada error yang terjadi
+    var_dump($exception->getTrace());
+} finally {
+    echo "Tetap dijalankan walau error" . PHP_EOL;
+}
